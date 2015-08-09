d54c0ffe7cc4c8e7a87e545574b2c59cddd8dfdd
yuanzhaohao <yuanzhaohao@163.com>
2015-08-09 22:35:14 +0800

    rebuild

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-## todo-react: 使用react、flux、gulp、browserify构建的todo-list demo
+todo-react
+=====
+> 使用react、flux、gulp、browserify构建的todo-list demo
 
-### 使用gulp进行构建
+## Demo & Examples
 
-#### 安装npm依赖
-  `npm install`、`gulp`
+Live demo: [http://yuanzhaohao.com/lazyload/demo/](http://yuanzhaohao.com/lazyload/demo/)
 
-#### 查看demo
-执行`gulp server`，就会自动打开[http://localhost:3333](http://localhost:3333)
+To build the examples locally, run:
 
-#### 参考
+```
+npm install
+gulp server
+```
+
+Then your example([localhost:3000/demo/](localhost:3000/demo/)) will open automatically in browser.
+## 参考
 
 * [官方栗子](https://facebook.github.io/flux/docs/todo-list.html)
 * [Architecting React.js Apps with Flux](http://tylermcginnis.com/reactjs-tutorial-pt-3-architecting-react-js-apps-with-flux/)
--- gulpfile.js
+++ gulpfile.js
@@ -22,29 +22,29 @@ gulp.task('js', function () {
   return bundler
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('demo'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('dist', ['js'], function () {
-  return gulp.src(['./demo/bundle.js'])
+  return gulp.src(['./dist/bundle.js'])
       .pipe(rename(function (path) {
           path.basename = path.basename + '.min';
       }))
       .pipe(code.lint())
       .pipe(code.minify())
-      .pipe(gulp.dest('demo'));
+      .pipe(gulp.dest('dist'));
 });
 
 gulp.task('connect', function () {
   return connect.server({
-    root: ['./demo'],
+    root: ['./dist'],
     livereload: true,
-    port: '3333'
+    port: '3000'
   });
 });
 
 gulp.task('open', function () {
-  return gulp.src('./demo/index.html').pipe(open('', { url: 'http://localhost:3333'}));
+  return gulp.src('./dist/index.html').pipe(open('', { url: 'http://localhost:3000'}));
 });
 
 gulp.task('watch', function () {
